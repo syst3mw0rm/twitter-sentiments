259e471948ef91cbf58c0b8c7c0ee39fff98f1f6
Aamir Khan <syst3m.w0rm@gmail.com>
2012-02-11 08:57:11 +0530

    use since_id in tweet fetching

--- httpclient.py
+++ httpclient.py
@@ -1,29 +1,39 @@
 import tornado.ioloop
 import tornado.httpclient
 import urllib
+import json
+import pika
+import sys
+
+
 
 url = "http://search.twitter.com/search.json?"
 
-parameters = [ ('q', '#facebook') , ('since', '136536013832069120') ]
-
-query = urllib.urlencode(parameters)
-
-url += query
+since_id = ""
 
 def handle_request(response):
+    global since_id
     if response.error:
         print "Error:", response.error
     else:
-        print response.body
+        json_object = json.loads(response.body)
+        since_id = json_object["max_id"]
+        print "1" #response.body
+    
+
 
 _http_client = tornado.httpclient.AsyncHTTPClient()
 
 def make_request():
-	_http_client.fetch(url, handle_request)
+	parameters = [ ('q', '#facebook'), ('rpp',100), ('include_entities', 'true'),
+			   ('since_id', since_id) ]
+	query = urllib.urlencode(parameters)
+	#print query
+	_http_client.fetch(url+query, handle_request)
 
 
 io_loop = tornado.ioloop.IOLoop.instance()
 
-schedule = tornado.ioloop.PeriodicCallback(make_request, 100)
+schedule = tornado.ioloop.PeriodicCallback(make_request, 1000)
 schedule.start()
 io_loop.start()
