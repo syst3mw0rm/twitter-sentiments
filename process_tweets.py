d280157b877c429b8e56a9f2b733206f195d5ae7
Aamir Khan <syst3m.w0rm@gmail.com>
2012-02-13 00:22:39 +0530

    update process_tweets

--- process_tweets.py
+++ process_tweets.py
@@ -4,8 +4,9 @@ import urllib
 import json
 import pika
 import sys
+from django.utils.encoding import smart_str
 
-textProcessingUrl = "http://api.text-processing.com/sentiment/"
+textProcessingUrl = "http://text-processing.com/api/sentiment/"
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
@@ -14,22 +15,30 @@ channel = connection.channel()
 channel.queue_declare(queue='tweets')
 
 def handle_request(response):
-	print response
+	print response.body
+	print response.headers
+	#print "got response"
 
 _http_client = tornado.httpclient.AsyncHTTPClient()
 
-def sentiment_analysis(ch, method, properties, text):
-	parameters = {"text" : text}
+def sentiment_analysis(ch, method, properties, json_body):
+	json_object = json.loads(json_body)
+        tweets = json_object["results"]
 	headers = {'Content-Type'   : 'application/x-www-form-urlencoded'}
-	query = urllib.urlencode(parameters)
-	print query
-
-	req_obj = tornado.httpclient.HTTPRequest(textProcessingUrl, 
-						 method = 'POST',
-						 body = query,
-						 headers = headers)
+	#print tweets
+	for tweet in tweets:
+		text = smart_str(tweet["text"])
+		parameters = {"text" : text}
+		#print tweet["text"]
+		headers = {'Content-Type'   : 'application/x-www-form-urlencoded'}
+		query = urllib.urlencode(parameters)
+		#print query
+		req_obj = tornado.httpclient.HTTPRequest(textProcessingUrl, 
+							 method = 'POST',
+							 body = query,
+							 headers = headers)
 	
-	_http_client.fetch(req_obj, handle_request)
+		_http_client.fetch(req_obj, handle_request)
 
 def consume_tweet():
 	channel.basic_consume(sentiment_analysis,
